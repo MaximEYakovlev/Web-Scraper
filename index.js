a0f2b27c788446aebc519927eece714f7f449813
Maxim Yakovlev <it.trainee@yandex.ru>
2022-08-22 11:59:44 +0300

    created a web scraper

--- index.js
+++ index.js
@@ -7,4 +7,24 @@ const PORT = process.env.PORT || 8001;
 
 const app = express();
 
+const url = "https://www.theguardian.com/uk";
+
+axios(url)
+  .then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const articles = [];
+
+    $(".fc-item__title", html).each(function () {
+      const title = $(this).text();
+      const url = $(this).find("a").attr("href");
+      articles.push({
+        title,
+        url,
+      });
+    });
+    console.log(articles);
+  })
+  .catch((err) => console.log(err));
+
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
